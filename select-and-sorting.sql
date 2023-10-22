e1e33e3fbc8893d3f7cb2dbff996825f0d1fc365
Kyle Matthews <linescrawler@gmail.com>
2023-10-22 19:53:55 +0100

    Adds titles, year of release and length of entries in the film table. Comments out task 1 for easier running.

---
+++ select-and-sorting.sql
@@ -0,0 +1,6 @@
+-- 1. Initial selection of actors from the database.
+--SELECT * FROM actor;
+
+-- 2. Return titles, year of release and length of all firms in database.
+SELECT title, release_year, length FROM film;
+
