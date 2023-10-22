29d36598322530ac13b68cd28e292cc8d23825ec
Kyle Matthews <linescrawler@gmail.com>
2023-10-22 19:58:53 +0100

    Adds script to return all information of 30 customers.

--- select-and-sorting.sql
+++ select-and-sorting.sql
@@ -2,5 +2,8 @@
 --SELECT * FROM actor;
 
 -- 2. Return titles, year of release and length of all firms in database.
-SELECT title, release_year, length FROM film;
+--SELECT title, release_year, length FROM film;
 
+-- 3. Return all info of 30 customers. 
+SELECT * FROM customer
+LIMIT 30;
