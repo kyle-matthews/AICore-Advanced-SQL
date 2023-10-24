227a497acc88b948d565be4f9b6dd41ed3ca71da
Kyle Matthews <linescrawler@gmail.com>
2023-10-24 17:28:53 +0100

    Uses multiple tables to filter and return customers from the United States.

--- SQL-join-types/us-customers.sql
+++ SQL-join-types/us-customers.sql
@@ -8,9 +8,18 @@ Filter the data to only get customer which live in the United States
 SELECT
     first_name,
     last_name,
-    address.address
+    address.address,
+    city.city,
+    country.country
 FROM
     customer
 
 INNER JOIN
-    address ON address.address_id = customer.address_id;
+    address ON address.address_id = customer.address_id
+INNER JOIN 
+    city ON city.city_id = address.city_id
+INNER JOIN
+    country ON country.country_id = city.city_id
+
+WHERE
+    country = 'United States';
