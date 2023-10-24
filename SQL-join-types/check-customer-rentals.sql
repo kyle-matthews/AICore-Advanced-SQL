bf9ae8a1f0bd6358f44d99ab5ac2e185970e5062
Kyle Matthews <linescrawler@gmail.com>
2023-10-24 16:50:25 +0100

    Initial commit.

---
+++ SQL-join-types/check-customer-rentals.sql
@@ -0,0 +1,5 @@
+/* 
+    1.Join the customer table to the rental table to check when customers were making rentals
+    2.Now join the inventory table to get the film_ids of the films rented
+    3.Now join the film table to get the names of all films rented by customers
+/*
